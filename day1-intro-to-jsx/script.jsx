eb090ca25fab4ebe4e5f6d829c6d51b4bd1fe5c4
samuelhutauruk2 <samuelhutauruk98@gmail.com>
2025-04-21 19:19:41 +0700

    commit modified script

--- day1-intro-to-jsx/script.jsx
+++ day1-intro-to-jsx/script.jsx
@@ -9,20 +9,30 @@
 // Creating components
 // In React, components are functions. Inside your script tag, create a new function called header:
 
-const app = document.getElementById('app');
 function Header() {
-    return <h1>Develop. Preview. Ship.</h1>;
+    // With JSX you can write expressions inside curly braces { }.
+    // The expression can be a React variable, or property, or any other valid JavaScript expression. 
+    // JSX will execute the expression and return the result:
+    return <h1>React is {5 + 5} times better with JSX</h1>;
 }
 
-// Nested components
+// Nested components :
+// One Top Level Element
+// The HTML code must be wrapped in ONE top level element.
+// Alternatively, you can use a "fragment" to wrap multiple lines. This will prevent unnecessarily adding extra nodes to the DOM.
+// A fragment looks like an empty HTML tag: <></>.
 function HomePage() {
     return (
         <>
+        {/* Elements Must be Closed :
+        JSX follows XML rules, and therefore HTML elements must be properly closed.
+        Close empty elements with '/>' */}
         <Header />
         <Header />
         </>
     );
 }
 
+const app = document.getElementById('app');
 const root = ReactDOM.createRoot(app);
 root.render(<HomePage />);
